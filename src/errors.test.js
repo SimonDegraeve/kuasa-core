2c9bbcc0aad09812230d7ad5bb015f954d254e16
Simon Degraeve <simon.degraeve@gmail.com>
2016-10-14 15:04:18 +0800

    noop: Cleanup tests

--- src/errors.test.js
+++ src/errors.test.js
@@ -8,7 +8,7 @@ import { HttpError, ServerError } from './errors';
  *
  */
 describe('Errors', () => {
-  it('creates a base HttpError', () => {
+  it('creates a basic HttpError', () => {
     const error = new HttpError();
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('');
@@ -18,13 +18,21 @@ describe('Errors', () => {
     expect(error.headers).toEqual({});
   });
 
-  it('creates a ServerError', () => {
-    const error = new ServerError('Something happened.');
+  it('creates a basic HttpError with message and headers', () => {
+    const error = new HttpError('Something happened.', { headers: { HeaderKey: 'Header Value' } });
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('Something happened.');
+    expect(error.statusCode).toBe(500);
+    expect(error.statusMessage).toBe('Internal Server Error');
+    expect(error.rfcName).toBe(undefined);
+    expect(error.headers).toEqual({ headerkey: 'Header Value' });
+  });
+
+  it('creates a ServerError', () => {
+    const error = new ServerError();
+    expect(error).toBeInstanceOf(Error);
     expect(error.statusCode).toBe(503);
     expect(error.statusMessage).toBe('Service Unavailable');
     expect(error.rfcName).toBe('server_error');
-    expect(error.headers).toEqual({});
   });
 });
